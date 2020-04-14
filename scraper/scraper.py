073efa30c97a951200bbe47e21152ac82519c8b5
isareds <ir.isacco.rossi@gmail.com>
2020-04-14 20:17:49 +0200

    DEV | Scraper now get Ateco Codes identification code and produce json file in which ateco structures is fully described

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,18 +1,184 @@
 import requests
-import logging
+import json
 from bs4 import BeautifulSoup
 
 """
  Variables that are preced by ** _ ** char are bs4 object
 """
 
-ATECO_CODES_PAGE_URL = 'https://www.codiceateco.it/sezioni'
+ATECO_CODES_PAGE_URL = 'https://www.codiceateco.it'
+ATECO_CODES_PAGE_SECTION_URL = 'https://www.codiceateco.it/sezioni'
 
 
 def scrape():
-    ateco_main_page = requests.get(ATECO_CODES_PAGE_URL)
-
+    ateco_main_page = requests.get(ATECO_CODES_PAGE_SECTION_URL)
     _ateco_main_page = BeautifulSoup(ateco_main_page.content, 'html.parser')
-    __ateco_main_page = _ateco_main_page.prettify()
+    
+    sections = parse_page(_ateco_main_page)
+    
+    # per ogni sezione mi calcolo le divisioni
+    for section in sections:
 
-    print(__ateco_main_page)
+        division_page = requests.get(ATECO_CODES_PAGE_URL + section['link'])
+        _division_page = BeautifulSoup(division_page.content, 'html.parser')
+        section['divisions'] = parse_page(_division_page)
+        
+        # per ogni divisione calcolo i gruppi 
+        for division in section['divisions']:
+
+            group_page = requests.get(ATECO_CODES_PAGE_URL + division['link'])
+            _group_page = BeautifulSoup(group_page.content, 'html.parser')
+            division['groups'] = parse_page(_group_page)
+            
+            # per ogni gruppo calcolo le classi
+            for group in division['groups']:
+
+                group_page = requests.get(ATECO_CODES_PAGE_URL + group['link'])
+                _group_page = BeautifulSoup(group_page.content, 'html.parser')
+                group['classes'] = parse_page(_group_page)
+                
+                # per ogni classe recupero i codici ateco
+                for a_class in group['classes']:
+                    
+                    ateco_page = requests.get(ATECO_CODES_PAGE_URL + a_class['link'])
+                    _ateco_page = BeautifulSoup(ateco_page.content, 'html.parser')
+                    a_class['codes'] = parse_page(_ateco_page)
+
+                    for ateco in a_class['codes']:
+                        print('Section[{}] | Division[{}] | Group[{}] | Class[{}] | Code[{}]'.format(section['code'], division['code'], group['code'], a_class['code'], ateco['code']))
+
+    with open('ateco_code.json', 'w') as outfile:
+        json.dump(sections, outfile)
+
+
+def parse_page(soup):
+    data = []
+    
+    table = soup.find('table')
+    rows = table.find_all('tr')
+ 
+    for row in rows:
+        cols = row.find_all('td')
+        parsed_cols = [ele.text.strip() for ele in cols]
+
+        info = {
+                'code': parsed_cols[0],
+                'name': parsed_cols[1],
+                'link': None,
+                }
+        
+        # cerco il link
+        for col in cols:
+            for a in col.find_all('a', href=True):
+                info['link'] = a['href']
+        
+        data.append(info)
+        
+        # data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
+
+def parse_division(soup):
+    data = []
+    
+    table = soup.find('table')
+    rows = table.find_all('tr')
+ 
+    for row in rows:
+        cols = row.find_all('td')
+        parsed_cols = [ele.text.strip() for ele in cols]
+
+        info = {
+                'code': parsed_cols[0],
+                'name': parsed_cols[1],
+                'link': None,
+                'groups': []
+                }
+        
+        # cerco il link
+        for col in cols:
+            for a in col.find_all('a', href=True):
+                info['link'] = a['href']
+        
+        data.append(info)
+        
+        # data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
+
+def parse_group(soup):
+    data = []
+    
+    table = soup.find('table')
+    rows = table.find_all('tr')
+ 
+    for row in rows:
+        cols = row.find_all('td')
+        parsed_cols = [ele.text.strip() for ele in cols]
+
+        info = {
+                'code': parsed_cols[0],
+                'name': parsed_cols[1],
+                'link': None,
+                }
+        
+        # cerco il link
+        for col in cols:
+            for a in col.find_all('a', href=True):
+                info['link'] = a['href']
+        
+        data.append(info)
+        
+        # data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
+
+def parse_class(soup):
+    data = []
+    
+    table = soup.find('table')
+    rows = table.find_all('tr')
+ 
+    for row in rows:
+        cols = row.find_all('td')
+        parsed_cols = [ele.text.strip() for ele in cols]
+
+        info = {
+                'code': parsed_cols[0],
+                'name': parsed_cols[1],
+                'link': None,
+                'codes': []
+                }
+        
+        # cerco il link
+        for col in cols:
+            for a in col.find_all('a', href=True):
+                info['link'] = a['href']
+        
+        data.append(info)
+        
+        # data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
+
+def parse_code(soup):
+    data = []
+    
+    table = soup.find('table')
+    rows = table.find_all('tr')
+ 
+    for row in rows:
+        cols = row.find_all('td')
+        parsed_cols = [ele.text.strip() for ele in cols]
+
+        info = {
+                'code': parsed_cols[0],
+                'name': parsed_cols[1],
+                'link': None,
+                }
+        
+        # cerco il link
+        for col in cols:
+            for a in col.find_all('a', href=True):
+                info['link'] = a['href']
+        
+        data.append(info)
+        
+        # data.append([ele for ele in cols if ele]) # Get rid of empty values
+    return data
