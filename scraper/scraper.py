9b1e714c2e0ebaa9f2a3ca563099bc739c9d7ac5
isareds <ir.isacco.rossi@gmail.com>
2020-04-15 11:54:26 +0200

    DEV | Scraper now generate two files one container ateco full structures and another contain only ateco codes

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -18,37 +18,40 @@ def scrape():
     
     # per ogni sezione mi calcolo le divisioni
     for section in sections:
-
         division_page = requests.get(ATECO_CODES_PAGE_URL + section['link'])
         _division_page = BeautifulSoup(division_page.content, 'html.parser')
         section['divisions'] = parse_page(_division_page)
         
         # per ogni divisione calcolo i gruppi 
         for division in section['divisions']:
-
             group_page = requests.get(ATECO_CODES_PAGE_URL + division['link'])
             _group_page = BeautifulSoup(group_page.content, 'html.parser')
             division['groups'] = parse_page(_group_page)
             
             # per ogni gruppo calcolo le classi
             for group in division['groups']:
-
                 group_page = requests.get(ATECO_CODES_PAGE_URL + group['link'])
                 _group_page = BeautifulSoup(group_page.content, 'html.parser')
                 group['classes'] = parse_page(_group_page)
                 
                 # per ogni classe recupero i codici ateco
                 for a_class in group['classes']:
-                    
                     ateco_page = requests.get(ATECO_CODES_PAGE_URL + a_class['link'])
                     _ateco_page = BeautifulSoup(ateco_page.content, 'html.parser')
                     a_class['codes'] = parse_page(_ateco_page)
 
                     for ateco in a_class['codes']:
+                        ateco = get_ateco_code_info(ateco)
                         print('Section[{}] | Division[{}] | Group[{}] | Class[{}] | Code[{}]'.format(section['code'], division['code'], group['code'], a_class['code'], ateco['code']))
 
-    with open('ateco_code.json', 'w') as outfile:
+    ateco_codes = get_only_ateco_codes(sections)
+
+    with open('ateco_codes_structure.json', 'w') as outfile:
         json.dump(sections, outfile)
+    
+    with open('ateco_codes.json', 'w') as outfile:
+        json.dump(ateco_codes, outfile)
+    
 
 
 def parse_page(soup):
@@ -76,3 +79,44 @@ def parse_page(soup):
         
         # data.append([ele for ele in cols if ele]) # Get rid of empty values
     return data
+
+def get_ateco_code_info(ateco_object):
+    ateco_code_page = requests.get(ATECO_CODES_PAGE_URL + ateco_object['link'])
+    _ateco_code_page = BeautifulSoup(ateco_code_page.content, 'html.parser')
+    
+    page = _ateco_code_page.find('section', class_='ateco-result')
+    if page is None:
+        return ateco_object
+
+    description = page.find('ul')
+    if description is None:
+        return ateco_object
+
+    description_list = [li.text.strip() for li in description.find_all('li')]
+    ateco_object['description'] = description_list
+    return ateco_object
+
+def get_only_ateco_codes(ateco_strucures, append_structure_info = False):
+    data = []
+    for section in ateco_strucures:
+        for division in section['divisions']:
+            for group in division['groups']:
+                for _class in group['classes']:
+                    for ateco in _class['codes']:
+                        print(ateco)
+                        ateco_obj = {
+                                'name': ateco['name'], 
+                                'ateco': ateco['code'], 
+                                'description': '\n'.join(ateco.get('description', '')), 
+                            }
+
+                        if append_structure_info:
+                            ateco_obj.update({
+                                    'section': section['code'],
+                                    'division': division['code'],
+                                    'group': group['code'],
+                                    'class': group['code'],
+                                })
+                        data.append(ateco_obj)
+    return data
+
